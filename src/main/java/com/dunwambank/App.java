55c80c42132923c00ceb10188bfcba981c58c1dd
Duncan472 <fduncan472@gamail.com>
2024-10-18 15:14:32 +0300

    n

--- src/main/java/com/dunwambank/App.java
+++ src/main/java/com/dunwambank/App.java
@@ -1,4 +1,18 @@
 package com.dunwambank;
 
-public class App {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader= new FXMLLoader(getClass().getResource( name; "/Fxml/Login.fxml"))
+        Scene scene = new Scene(FXMLLoader.load())
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
