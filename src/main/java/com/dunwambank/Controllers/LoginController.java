f3f4c7b2ab99a4e57ffef916a2d4500ee7e37886
Duncan472 <fduncan472@gmail.com>
2025-02-27 10:37:30 +0300

    Created the LoginMethod

--- src/main/java/com/dunwambank/Controllers/LoginController.java
+++ src/main/java/com/dunwambank/Controllers/LoginController.java
@@ -1,22 +1,36 @@
 package com.dunwambank.Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 
 import java.util.Objects;
 
 public class LoginController  {
 
-
+    @FXML
+    public Hyperlink ForgotPasswordHyperlink;
+    @FXML
+    public Button loginButton;
+    @FXML
+    public Button cancelButton;
+    @FXML
+    public Hyperlink forgotPasswordHyperlink;
+    @FXML
+    public Hyperlink helpHyperlink;
+    @FXML
+    public TextField payeeAddressTextField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public ChoiceBox choiceBoxAccount;
     @FXML
     private ImageView loginImage;
 
-    public ChoiceBox acc_selector;
+//    public ChoiceBox acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
     public TextField password_fld;
@@ -29,8 +43,12 @@ public class LoginController  {
     }
 
     private void initializeUiElements() {
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/Home.png")));
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/icons8-bank-100.png")));
         loginImage.setImage(image);
     }
+
+    public void cancelOnAction() {
+        Stage window = (Stage) cancelButton.getScene().getWindow();
+        window.close();
 
 }
