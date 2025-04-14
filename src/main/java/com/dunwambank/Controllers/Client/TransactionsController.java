1fd4641aa8de4da809e2375c26ba3b2c0a46b2b3
Duncan472 <fduncan472@gmail.com>
2025-04-14 23:31:59 +0300

    M

--- src/main/java/com/dunwambank/Controllers/Client/TransactionsController.java
+++ src/main/java/com/dunwambank/Controllers/Client/TransactionsController.java
@@ -1,4 +1,12 @@
 package com.dunwambank.Controllers.Client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+
+
 public class TransactionsController {
+        @FXML
+        private ListView<?> transactionsListView;
+
 }
