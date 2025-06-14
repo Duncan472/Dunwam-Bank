0c8f4551d767d032d5159eedf5b3bf9b1658840c
Duncan472 <fduncan472@gmail.com>
2025-06-14 22:35:58 +0300

    Modified the admin menu and styled iit

--- src/main/java/com/dunwambank/Controllers/Admin/AdminMenuController.java
+++ src/main/java/com/dunwambank/Controllers/Admin/AdminMenuController.java
@@ -1,4 +1,27 @@
 package com.dunwambank.Controllers.Admin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class AdminMenuController {
+
+    @FXML
+    private Button DepositButton;
+
+    @FXML
+    private Button clientsButton;
+
+    @FXML
+    private Button createClientButton;
+
+    @FXML
+    private Button logoutButton;
+
+
+//    @Override
+//    public void initializable(URL url, ResourceBundle resourceBundle){}
+
 }
