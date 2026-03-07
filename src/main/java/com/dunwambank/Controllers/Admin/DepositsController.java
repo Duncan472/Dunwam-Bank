8e3e7b71ce369d7d086481b8523636f5b5fc8dec
Duncan472 <fduncan472@gmail.com>
2026-03-07 23:57:32 +0300

    Created the id's for the deposits controller

--- src/main/java/com/dunwambank/Controllers/Admin/DepositsController.java
+++ src/main/java/com/dunwambank/Controllers/Admin/DepositsController.java
@@ -1,10 +1,24 @@
 package com.dunwambank.Controllers.Admin;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 public class DepositsController implements Initializable {
+    @FXML
+    private TextField amount_field;
+    @FXML
+    private Button deposit_button;
+    @FXML
+    private TextField payeeAddress_field;
+    @FXML
+    private ListView<?> resultsListview;
+    @FXML
+    private Button search_button;
     @Override
     public void initialize(java.net.URL url, java.util.ResourceBundle resourceBundle) {
+
 
     }
 }
